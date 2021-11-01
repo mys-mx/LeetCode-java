09dd70dedb5dac673978aef8820dbaff854a4600
mys-mx <13373338337@163.com>
2021-11-02 07:37:08 +0800

    爬楼梯问题(菲波那切数列)增加去重递归方法

--- src/main/java/com/amos/code/LeetCode9_ClimbStairs.java
+++ src/main/java/com/amos/code/LeetCode9_ClimbStairs.java
@@ -1,5 +1,8 @@
 package com.amos.code;
 
+import java.util.Arrays;
+import java.util.PrimitiveIterator;
+
 /**
  * @program: leetcode_test
  * @description: 爬楼梯:F(n)=F(n-1)+F(n-2)
@@ -7,7 +10,9 @@ package com.amos.code;
  */
 public class LeetCode9_ClimbStairs {
     public static void main(String[] args) {
+        System.out.println(ClimbStairs3(4));
         System.out.println(ClimbStairs2(4));
+        System.out.println(ClimbStairs1(4));
 
     }
 
@@ -27,11 +32,27 @@ public class LeetCode9_ClimbStairs {
     }
 
 
-    //递归   时间复杂度O(2^n)
+    //暴力递归   时间复杂度O(2^n)
     public static int ClimbStairs2(int n) {
         if (n <= 1) return 1;
         return ClimbStairs2(n - 1) + ClimbStairs2(n - 2);
     }
 
+    //去重递归 时间复杂度O(n) 空间复杂度是O(n)
+    public static int ClimbStairs3(int n) {
+        int[] arr = new int[n + 1];
+        PrimitiveIterator.OfInt iterator = Arrays.stream(arr).iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        return resent(arr, n);
+    }
 
+    public static int resent(int[] arr, int n) {
+        if (n <= 1) return 1;
+        if (arr[n] != 0) {
+            return arr[n];
+        }
+        return resent(arr, n - 1) + resent(arr, n - 2);
+    }
 }
